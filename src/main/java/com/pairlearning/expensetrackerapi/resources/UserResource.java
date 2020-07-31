56532122a2567d71a38dbbc40b421901d7447250
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-31 02:35:30 +0100

    creating api for users

---
+++ src/main/java/com/pairlearning/expensetrackerapi/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.pairlearning.expensetrackerapi.resources;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserResource {
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody Map<String, Object> userMap) {
+        String firstName = (String) userMap.get("firstName");
+
+    }
+}
