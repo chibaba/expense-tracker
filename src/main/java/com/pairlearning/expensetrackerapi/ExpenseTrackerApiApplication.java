4d8a4fc90b02d0f063151d6f519975773cb96378
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-31 01:44:08 +0100

    setting up databases for docker for postgresql

---
+++ src/main/java/com/pairlearning/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pairlearning.expensetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+
+}
