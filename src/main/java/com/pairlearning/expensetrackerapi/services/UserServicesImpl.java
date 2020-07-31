39d13f486b9f91a15033051a37e8409c6e0ac7ae
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-31 20:38:37 +0100

    wip

--- src/main/java/com/pairlearning/expensetrackerapi/services/UserServicesImpl.java
+++ src/main/java/com/pairlearning/expensetrackerapi/services/UserServicesImpl.java
@@ -2,7 +2,12 @@ package com.pairlearning.expensetrackerapi.services;
 
 import com.pairlearning.expensetrackerapi.domain.User;
 import com.pairlearning.expensetrackerapi.exception.EtAuthException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+@Service
+@Transactional
 public class UserServicesImpl implements UserServices {
 
     @Override
