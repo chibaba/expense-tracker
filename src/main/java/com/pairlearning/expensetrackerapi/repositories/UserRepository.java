f11f21050a6ba459e36ea7ba964decf3b06c8f19
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-31 20:14:35 +0100

    create repository for user

---
+++ src/main/java/com/pairlearning/expensetrackerapi/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pairlearning.expensetrackerapi.repositories;
+
+import com.pairlearning.expensetrackerapi.domain.User;
+import com.pairlearning.expensetrackerapi.exception.EtAuthException;
+
+public interface UserRepository {
+    Integer create(String firstName, String lastName, String email, String password) throws EtAuthException;
+
+    User findByEmailAndPassword(String Email, String Password) throws EtAuthException;
+
+    Integer getCountByEmail(String email);
+}
